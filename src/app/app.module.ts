3d814fbea879163c1783143e74794321b82e7253
Quinten <90308786+QNoah@users.noreply.github.com>
2023-09-13 09:47:17 +0200

    app component added with more selectors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,30 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/pages/home/home.component';
-import {MatIconModule} from '@angular/material/icon';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { NavigationComponent } from './components/pages/home/navigation/navigation.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SkillsComponent } from './components/pages/home/skills/skills.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProjectsComponent, NavigationComponent, PageNotFoundComponent, AboutComponent, ContactComponent, FooterComponent, SkillsComponent, ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
