7de4c83b9e0b62d8b91920ce9343bc4b8349544d
Quinten <90308786+QNoah@users.noreply.github.com>
2023-09-13 08:15:06 +0200

    Added Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
